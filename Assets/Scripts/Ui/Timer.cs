840f2d5f6dbd29500259f23dd6ea5990dde90d6d
SHUO HUANG <1806163@uad.ac.uk>
2019-10-07 15:54:35 +0100

    timer
    timer finished

---
+++ Assets/Scripts/Ui/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = 20 - (Time.time - startTime);
+        if ( t < 10)
+        {
+            timerText.color = Color.red;
+            if (t <= 0)
+                return;
+        }
+       
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f1");
+        timerText.text = minutes + ":" + seconds;
+    }
+}
