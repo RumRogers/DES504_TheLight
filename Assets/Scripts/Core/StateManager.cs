cc99e94547592a0081870428bfe529ecf5d955d7
ANDREA SERRELI <1900257@uad.ac.uk>
2019-10-08 10:52:10 +0100

    Wrench usage implemented + StateManager change
    - Implemented pipe rotation when player has the wrench as current item and presses action in the pipe's trigger zone.
    - All resettable items have now a proper tag, so that the StateManager can fill its own list of game objects to reset.

--- Assets/Scripts/Core/StateManager.cs
+++ Assets/Scripts/Core/StateManager.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class StateManager : MonoBehaviour
 {
     [SerializeField] private List<Resettable> m_resettables = new List<Resettable>();
-    
+
+    private void Awake()
+    {
+        GameObject[] resettableObjects = GameObject.FindGameObjectsWithTag("Resettable");
+        for(int i = 0; i < resettableObjects.Length; i++)
+        {
+            m_resettables.Add(resettableObjects[i].GetComponent<Resettable>());
+        }
+    }
     public void ResetAll()
     {
         foreach(Resettable resettable in m_resettables)
---
+++ Assets/Scripts/Puzzle/Pipe.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public enum PipeDirection
+    {
+        Left,
+        Right
+    }
+
+    private static float rotation_speed = 100;
+
+    [SerializeField] private PipeDirection m_direction;
+    [SerializeField] private Transform m_pipeJumpHotspot;
+    [SerializeField] private Transform m_pipeEnd;
+    [SerializeField] private bool m_isStuck = false;
+    private PlayerController m_playerController;
+    private bool m_rotating = false;
+
+    private void Awake()
+    {
+
+        GameObject gameObject = GameObject.Find("Player");
+        if(gameObject != null)
+        {
+            m_playerController = gameObject.GetComponent<PlayerController>();
+        }      
+    }
+
+    private void Update()
+    {
+        if(m_direction == PipeDirection.Left && transform.rotation.eulerAngles.y != 0)
+        {
+            Rotate(-1);
+        }
+        else if(m_direction == PipeDirection.Right && transform.rotation.eulerAngles.y != 180)
+        {
+            Rotate(1);
+        }
+    }
+
+    private void Rotate(int dir)
+    {
+        transform.Rotate(0, rotation_speed * dir * Time.deltaTime, 0, Space.Self);
+
+        if(transform.rotation.eulerAngles.y > 270)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            m_rotating = false;
+        }
+        else if(transform.rotation.eulerAngles.y > 180)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            m_rotating = false;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(Input.GetButtonDown("Action") && !m_rotating)
+            {
+                switch(m_playerController.CurrentItem)
+                {
+                    case Inventory.InventoryItems.None:
+                        StartCoroutine(m_playerController.GrabPipe(m_pipeJumpHotspot.position, m_pipeEnd.position, m_direction));
+                        break;
+                    case Inventory.InventoryItems.MonkeyWrench:
+                        m_rotating = true;
+                        m_direction = m_direction == PipeDirection.Left ? PipeDirection.Right : PipeDirection.Left;
+                        break;
+                }
+                
+            }
+        }
+    }
+}
