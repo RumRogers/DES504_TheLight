9c0a238c07e874f494d4aa35528cbaeecaf81b69
ANDREA SERRELI <1900257@uad.ac.uk>
2019-10-17 15:44:40 +0100

    Added Main Menu

---
+++ Assets/Scripts/UI/MainMenu/RandomizeCredits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeCredits : MonoBehaviour
+{
+    [SerializeField] private string[] m_members = new string[4];
+    [SerializeField] private TMPro.TextMeshProUGUI[] m_labels = new TMPro.TextMeshProUGUI[4];
+    private Dictionary<TMPro.TextMeshProUGUI, bool> m_assignedCouples;
+
+
+
+    private void Awake()
+    {
+        //RandomizeCouples();
+    }
+
+    public void RandomizeCouples()
+    {
+        m_assignedCouples = new Dictionary<TMPro.TextMeshProUGUI, bool>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            string member = m_members[i];
+            int labelIdx;
+
+            do
+            {
+                labelIdx = Random.Range(0, 4);
+            }
+            while (m_assignedCouples.ContainsKey(m_labels[labelIdx]));
+
+            m_assignedCouples[m_labels[labelIdx]] = true;
+            m_labels[labelIdx].text = member;
+        }
+    }
+}
