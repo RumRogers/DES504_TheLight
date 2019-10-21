99505496450eab061c043b473f7c73d602ad3a60
ANDREA SERRELI <1900257@uad.ac.uk>
2019-10-21 15:51:51 +0100

    Cameras/cops cone of view drawn in-game

--- Assets/Scripts/Stealth/CCTVCamera.cs
+++ Assets/Scripts/Stealth/CCTVCamera.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class CCTVCamera : MonoBehaviour
 {
-    [SerializeField] private bool m_debugMode = false;
+    [SerializeField] private bool m_debugMode = true;
     private Transform m_lens;
     [SerializeField] private Transform m_target;
-    [SerializeField] private float m_distance = 5f;
-    [SerializeField] protected float m_fovDegrees = 90;
-    [SerializeField] protected bool m_alarm = false;
+    [SerializeField] private float m_distance = 10f;
+    [SerializeField] protected float m_fovDegrees = 20;
+    [SerializeField] protected bool m_alarm = false;    
+    [SerializeField] private Vector3 axis;
     private Color m_nativeColor;
-   
+    private LineRenderer m_lineRenderer;
+
     public Transform Target { get { return m_target; } set { m_target = value;  } }
     private Vector3 m_viewconeBaseCenter;
 
 
     private List<MeshRenderer> m_meshRenderers = new List<MeshRenderer>();
+
     protected virtual void Awake()
     {
         m_lens = transform.Find("Lens");
@@ -30,8 +33,13 @@ public class CCTVCamera : MonoBehaviour
         {
             m_nativeColor = m_meshRenderers[0].material.color;
         }
-        
 
+        m_lineRenderer = GetComponent<LineRenderer>();
+        if(m_lineRenderer != null)
+        {
+            m_lineRenderer.positionCount = 720;
+            //m_lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        }
     }
 
     // Update is called once per frame
@@ -42,6 +50,12 @@ public class CCTVCamera : MonoBehaviour
         {
             DrawDebuggingGraphics();
         }
+
+        if(m_lineRenderer != null)
+        {
+            DrawCone();
+        }
+
         m_alarm = IsTargetVisible();
     }
 
@@ -60,8 +74,39 @@ public class CCTVCamera : MonoBehaviour
 
     protected void DrawDebuggingGraphics()
     {
+        //Vector3 a = Quaternion.Euler(0, 45, 0) * m_lens.forward;
+        //Vector3 a = Quaternion.Euler(axis.x, axis.y, axis.z) * (m_lens.position + m_lens.forward);
+        //Vector3 a = Quaternion.identity * (m_lens.position + m_lens.forward);
+        Vector3 a = Quaternion.Euler(0, m_fovDegrees / 2, 0) * m_lens.forward;
+        Vector3 b = Quaternion.Euler(0, -m_fovDegrees / 2, 0) * m_lens.forward;
+        Vector3 c = Quaternion.Euler(0, 0, m_fovDegrees / 2) * m_lens.forward;
+        Vector3 d = Quaternion.Euler(0, 0, -m_fovDegrees / 2) * m_lens.forward;
+        
+
+        /*Debug.DrawLine(m_lens.position, m_lens.position + a * m_distance, Color.red);
+        Debug.DrawLine(m_lens.position, m_lens.position + b * m_distance, Color.red);
         Debug.DrawLine(m_lens.position, m_lens.position + m_lens.forward * m_distance, Color.blue);
-        if(m_alarm)
+        Debug.DrawLine(m_lens.position, m_lens.position + c * m_distance, Color.red);
+        Debug.DrawLine(m_lens.position, m_lens.position + d * m_distance, Color.red);*/
+
+
+        Vector3 axis1 = Vector3.Lerp(a, b, .5f);
+        Vector3 axis2 = Vector3.Lerp(c, d, .5f);
+        axis = Vector3.Lerp(axis1, axis2, 0.5f);
+
+        Vector3 v;
+
+        for (int i = 0; i < 360; i++)
+        {
+
+            //f = Quaternion.Euler(0, m_fovDegrees / 2 - i, m_fovDegrees / 2 - i) * b;
+            //f = Quaternion.Euler(0, -m_fovDegrees / 2 + i, -m_fovDegrees / 2 + i) * m_lens.forward;
+            v = Quaternion.AngleAxis(i, axis) * b;
+            Debug.DrawLine(m_lens.position, m_lens.position + v * m_distance, Color.red);
+        }
+
+        //Debug.DrawLine(m_lens.position, m_lens.position + f * m_distance, Color.red);
+        if (m_alarm)
         {
             Debug.DrawLine(m_lens.position, m_target.position, Color.red);
         }
@@ -81,6 +126,28 @@ public class CCTVCamera : MonoBehaviour
             {
                 m_meshRenderers[i].material.color = m_nativeColor;
             }
+        }
+    }
+
+    private void DrawCone()
+    {
+        Vector3 a = Quaternion.Euler(0, m_fovDegrees / 2, 0) * m_lens.forward;
+        Vector3 b = Quaternion.Euler(0, -m_fovDegrees / 2, 0) * m_lens.forward;
+        Vector3 c = Quaternion.Euler(0, 0, m_fovDegrees / 2) * m_lens.forward;
+        Vector3 d = Quaternion.Euler(0, 0, -m_fovDegrees / 2) * m_lens.forward;
+
+        Vector3 axis1 = Vector3.Lerp(a, b, .5f);
+        Vector3 axis2 = Vector3.Lerp(c, d, .5f);
+        axis = Vector3.Lerp(axis1, axis2, 0.5f);
+
+        Vector3 v;
+
+        for (int i = 0; i < 720; i += 2)
+        {
+            m_lineRenderer.SetPosition(i, m_lens.position);
+
+            v = Quaternion.AngleAxis(i, axis) * b;
+            m_lineRenderer.SetPosition(i + 1, m_lens.position + v * m_distance);
         }
     }
 }
