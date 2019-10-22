879e4fdd20c472f5b7d22d8c3e5d5acd4a1c02dd
ANDREA SERRELI <1900257@uad.ac.uk>
2019-10-22 11:34:42 +0100

    Witness behavior fully implemented!
    A witness can sleepwalk (thus waking up intermittently) or simply get awaken by a cat meowing.

--- Assets/Scripts/Stealth/WitnessLightWindow.cs
+++ Assets/Scripts/Stealth/WitnessLightWindow.cs
@@ -6,23 +6,40 @@ public class WitnessLightWindow : MonoBehaviour
 {
     private Transform m_witnessBody;
     private Transform m_light;
+    private Transform m_balloon;
     private BoxCollider m_collider;
-    [SerializeField] private float m_asleepDuration = 60f;
-    [SerializeField] private float m_awokenDuration = 5f;
+    [SerializeField] private float m_asleepDuration = 10f;
+    [SerializeField] private float m_awakenDuration = 5f;
+    [SerializeField] private bool m_isSleepwalker = false;
     private bool m_gotcha = false;
 
+    public bool m_debugWakeup = false;
     private void Awake()
     {
         m_witnessBody = transform.GetChild(0);
         m_light = transform.GetChild(1);
+        m_balloon = transform.GetChild(2);
         m_collider = GetComponent<BoxCollider>();
+        m_balloon.gameObject.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Loop());
+        SetPeeking(false);
+        if(m_isSleepwalker)
+        {
+            StartCoroutine(SleepWalk());
+        }
     }
 
+    private void Update()
+    {
+        if (m_debugWakeup)
+        {
+            m_debugWakeup = false;
+            StartCoroutine(WakeUpOnce());
+        }
+    }
     void SetPeeking(bool peeking)
     {
         m_witnessBody.gameObject.SetActive(peeking);
@@ -30,32 +47,37 @@ public class WitnessLightWindow : MonoBehaviour
         m_collider.enabled = peeking;
     }
 
-    public IEnumerator Loop()
+    private IEnumerator SleepWalk()
     {
         while(true)
         {
             SetPeeking(false);
+            m_balloon.gameObject.SetActive(false);
             yield return new WaitForSeconds(m_asleepDuration);
             SetPeeking(true);
-            yield return new WaitForSeconds(m_awokenDuration);
+            yield return new WaitForSeconds(m_awakenDuration);
         }
+    }
+
+    public IEnumerator WakeUpOnce()
+    {
+        SetPeeking(true);
+        yield return new WaitForSeconds(m_awakenDuration);
+        SetPeeking(false);
+        m_balloon.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if(!m_gotcha)
+            m_balloon.gameObject.SetActive(true);
+            if (!m_gotcha)
             {
                 print("GOTCHA!");
                 m_gotcha = true;
                 // increase witnesses count
             }
-            else
-            {
-                print("Spotted again...");
-            }
-
         }
     }
 }
