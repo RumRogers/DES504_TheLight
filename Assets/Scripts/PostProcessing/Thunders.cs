fff422b52c1a2aed2db87d40916c1e1e436814f7
Andrea Serreli <andreaserreli1984@gmail.com>
2019-11-25 14:56:10 +0000

    Test new avatar

--- Assets/Scripts/PostProcessing/Thunders.cs
+++ Assets/Scripts/PostProcessing/Thunders.cs
@@ -11,6 +11,8 @@ public class Thunders : MonoBehaviour
     private bool m_throwing = false;
     [SerializeField] private float m_lightSpeedMultiplier = 1.5f;
     [SerializeField] private bool m_throwLightning = false;
+    [SerializeField] private float m_minThreshold = .3f; 
+    [SerializeField] private float m_goalThreshold = 1;
     
     private void Awake()
     {
@@ -22,8 +24,7 @@ public class Thunders : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(!m_throwing && Random.Range(0f, 1f) >= .85f)
-        if (Random.Range(0f, 1f) >= .98f)
+        if(!m_throwing && Random.Range(0f, 1f) >= .95f)        
         {
             m_throwLightning = true;
         }
@@ -31,29 +32,35 @@ public class Thunders : MonoBehaviour
         if(m_throwLightning)
         {
             m_throwLightning = false;
-            StartCoroutine(ThrowLightning());
+            StartCoroutine(DoThrow());
         }
     }
 
     private IEnumerator ThrowLightning()
     {
-        m_throwing = true;
-        m_bloomLayer.threshold.value = 0f;
-        if(!m_audioSource.isPlaying)
+        m_bloomLayer.threshold.value = m_minThreshold;
+        if (!m_audioSource.isPlaying)
         {
             m_audioSource.PlayOneShot(m_audioSource.clip);
         }
-        
 
-        while(m_bloomLayer.threshold.value < 1)
+
+        while (m_bloomLayer.threshold.value < m_goalThreshold)
         {
             yield return new WaitForSeconds(0.01f);
             m_bloomLayer.threshold.value += Time.deltaTime * m_lightSpeedMultiplier;
         }
 
-        m_bloomLayer.threshold.value = 1;
+        m_bloomLayer.threshold.value = m_goalThreshold;    
+    }
 
-        yield return new WaitForSeconds(Random.Range(1, 6));
+    private IEnumerator DoThrow()
+    {
+        m_throwing = true;
+        StartCoroutine(ThrowLightning());
+        yield return new WaitForSeconds(.1f);
+        StartCoroutine(ThrowLightning());
+        yield return new WaitForSeconds(Random.Range(10, 20));
         m_throwing = false;
     }
 }
